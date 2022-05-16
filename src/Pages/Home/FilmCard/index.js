e4e6f0bb28091b5e1e524b79dabb19c2d2c686dd
Gabriel Moslaves <gabrielmoslaves@gmail.com>
2022-05-16 17:29:57 -0300

    ajuste

--- src/Pages/Home/FilmCard/index.js
+++ src/Pages/Home/FilmCard/index.js
@@ -21,7 +21,7 @@ function FilmCard() {
       </div>
 
       <div className="movie-name">BATMAN</div>
-      <div className="movie-description">Um moguerço da pesada que vai se envolver em altas aventuras com essa galerinha da pesada de Gotham City</div>
+      <div className="movie-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus neque. Cras sit amet ligula ut justo commodo porta id ut enim. Nulla est lectus, mollis sit amet vehicula id, volutpat eget mauris.</div>
       <button className="watch-button">Assistir agora</button>
     </div>
   );
